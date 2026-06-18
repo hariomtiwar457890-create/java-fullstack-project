84acc49d97c3a766e78c7db37e8524a1f6b611a0
Hariom Tiwari <hariom457890@gmail.com>
2026-06-18 17:11:41 +0530

    project

---
+++ hariom/sum.java
@@ -0,0 +1,24 @@
+package hariom;
+import java.util.Scanner;
+public class sum {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.println("Sum: " + c);
+    }
+}
+
+       // int c = a + b;
+       //
+       
+       //System.out.println("Sum: " + c);
+       int a;
+       int b ;
+       
+    }
+}
