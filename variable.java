bed08e5161374b6069b49ce1a7b14cd8e19debd8
Hariom Tiwari <hariom457890@gmail.com>
2026-06-18 15:43:47 +0530

    Initial Upload

---
+++ variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[] args) {
+        
+        int a = 34;
+        int b = 45;
+        int  c = (a+b);
+        System.out.println(c);
+    }
+}
